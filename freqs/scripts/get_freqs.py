f1084623526e8c2d9d7f5e180cd2ef1e308ff0ba
Richard Futrell <canjo@dhcp-18-111-11-231.dyn.mit.edu>
2017-08-17 13:20:10 -0400

    update scripts

--- freqs/scripts/get_freqs.py
+++ freqs/scripts/get_freqs.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python2
 """ get frequency information for tokens in stories from Google Books """
 from __future__ import print_function
 import sys
--- naturalstories_RTS/analysis_of_batches.R
+++ naturalstories_RTS/analysis_of_batches.R
@@ -1,12 +1,14 @@
 library(plyr)
+library(dplyr)
 library(ggplot2)
 
 #read in RT data from 2 separate files
-d <- read.csv('batch2_pro.csv')
-d2 <- read.csv('batch1_pro.csv')
-d <- rbind(d, d2)
+b2 <- read.csv('batch2_pro.csv')
+b1 <- read.csv('batch1_pro.csv')
+d <- rbind(b1, b2)
 
-##subtract 2 from zone to properly align region...should confirm with Hal that this is correct, but the RTs seem to line up correctly in plots
+##subtract 2 from zone to properly align region...should confirm with Hal that this is correct,
+## but the RTs seem to line up correctly in plots
 d$zone <- d$zone - 2
 
 #read in story words and region
@@ -15,10 +17,11 @@ word.df <- read.csv('all_stories.tok', sep = '\t')
 d <- merge(d, word.df, by= c('item', 'zone'), all.x = T, all.y = T)
 
 #remove regions that do not have words
-d <- d[!is.na(d$word), ]
+d <- filter(d, !is.na(word))
 
 #exclude stories where subject does not get more than 4/6 correct
-d <- d[d$correct > 4, ]
+unfiltered <- d
+d <- filter(d, correct > 4)
 
 #exclude data points less than 50 ms, greater than 3000 ms
 d <- d[d$RT > 100 & d$RT < 3000, ]
@@ -30,13 +33,19 @@ d$l <- nchar(as.character(d$word))
 gmean <- function(x) exp(mean(log(x)))
 gsd   <- function(x) exp(sd(log(x)))
 
-word.info <- ddply(d , .(word, zone, item), summarise, nItem = length(RT), meanItemRT = mean(RT), sdItemRT = sd(RT), gmeanItemRT = gmean(RT), gsdItemRT = gsd(RT))
+word.info = d %>%
+  group_by(word, zone, item) %>%
+    summarise(nItem=length(RT),
+              meanItemRT=mean(RT),
+	      sdItemRT=sd(RT),
+	      gmeanItemRT=sd(RT),
+	      gsdItemRT=gsd(RT))
 
-d <- merge(d, word.info, by = c('word', 'zone', 'item'))
+d <- inner_join(d, word.info, by=c("word", "zone", "item"))
 
 #write processed output, by word, overall
-write.table(word.info, 'processed_wordinfo.tsv', quote = F, row.names=F, sep="\t")
-write.table(d, 'processed_RTs.tsv', quote=F, row.names=F, sep="\t")
+#write.table(word.info, 'processed_wordinfo.tsv', quote = F, row.names=F, sep="\t")
+#write.table(d, 'processed_RTs.tsv', quote=F, row.names=F, sep="\t")
 
 ggplot(d, aes(RT)) + facet_grid( . ~ WorkerId) + geom_histogram()
 
@@ -44,7 +53,9 @@ ggplot(d, aes(RT)) + facet_grid( . ~ WorkerId) + geom_histogram()
 ##make plot
 make.story.plot <- function(item, group)
 {
-return ( ggplot(word.info[word.info$item == item & word.info$group == group, ], aes(x = zone, y = meanItemRT, group = group)) + 
+return (
+  ggplot(word.info[word.info$item == item & word.info$group == group, ],
+  aes(x = zone, y = meanItemRT, group = group)) + 
   geom_line(colour = 'grey') +
   geom_text(aes( x = zone, y= meanItemRT, label = word), size = 2) + facet_grid(group~ .) +
   theme_bw() + coord_cartesian(ylim = c(min(word.info$meanItemRT), 550)))
@@ -56,3 +67,4 @@ pdf('practice_RTplot.pdf')
 for (i in seq(1:20))
 print(make.story.plot(1, i))
 dev.off()
+
