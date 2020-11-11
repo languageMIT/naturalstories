cab073f1fa674ca18dbc3021d87a679c766fb984
Futrell <subutai@mailinator.com>
2020-11-11 10:46:57 -0800

    Update RT alignment

---
+++ naturalstories_RTS/process_RTs.R
@@ -0,0 +1,35 @@
+rm(list=ls())
+library(tidyverse)
+
+b1 = read_csv("batch1_pro.csv")
+b2 = read_csv("batch2_pro.csv")
+words = read_tsv("all_stories.tok")
+
+b = bind_rows(b1, b2)
+
+offset = 1001
+
+d = b %>%
+    mutate(zone=if_else(item == 3 & zone > offset, zone - 3, zone - 2)) %>%
+    inner_join(words) %>%
+    filter(RT > 100, RT < 3000, correct > 4) %>%
+    arrange(item, zone)
+
+gmean <- function(x) exp(mean(log(x)))
+gsd   <- function(x) exp(sd(log(x)))
+
+wordinfo = d %>%
+    group_by(word, zone, item) %>%
+    summarize(nItem=n(),
+              meanItemRT=mean(RT),
+              sdItemRT=sd(RT),
+              gmeanItemRT=gmean(RT),
+              gsdItemRT=gsd(RT)) %>%
+    ungroup()
+
+write_tsv(wordinfo, "processed_wordinfo.tsv")
+
+d %>% inner_join(wordinfo) %>% write_tsv("processed_RTs.tsv")
+
+
+
